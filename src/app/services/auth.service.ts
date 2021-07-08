208c3e07306a82e04b9a416f33e0f85dd9e1d5ee
definite2 <edefne878@gmail.com>
2021-07-08 15:54:15 +0300

    add authentication injectable

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  authenticated$: Observable<boolean>;
+  uid$: Observable<string>;
+  user: Observable<firebase.User>;
+  constructor(
+    public fireAuth: AngularFireAuth,
+    public store: AngularFirestore,
+    public router: Router
+  ) {
+    this.authenticated$ = this.fireAuth.authState.pipe(map((user) => !!user));
+    this.user = fireAuth.authState;
+    this.uid$ = fireAuth.authState.pipe(map((user) => user.uid));
+  }
+  signIn(provider: firebase.auth.AuthProvider): Promise<any> {
+    return this.fireAuth.signInWithPopup(provider);
+  }
+  signInWithGoogle(): Promise<any> {
+    return this.signIn(new firebase.auth.GoogleAuthProvider());
+  }
+  signInWithGithub(): Promise<any> {
+    return this.signIn(new firebase.auth.GithubAuthProvider());
+  }
+  signOut() {
+    this.fireAuth.signOut().then((_) => this.router.navigate(['/']));
+  }
+}
